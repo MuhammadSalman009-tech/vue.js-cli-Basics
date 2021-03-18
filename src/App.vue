f7674d500896a77e6c8e7347edba636bd0611887
MuhammadSalman009-tech <salmansumra009@gmail.com>
2021-03-18 16:31:18 +0500

    vue/cli initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>this is body paragraph element</p>
+  <teleport to=".modals" v-if="isOpen">
+    <Modal :header="header" @closeModal="toggleModal">
+      <h1>this is first modal</h1>
+      <template v-slot:links>
+        <a href="/">Sign Up</a> <br><br>
+        <a href="/">Sign In</a>
+      </template>
+    </Modal>
+  </teleport>
+  <div v-if="isOpen2">
+    <Modal :header="header" @closeModal="toggleModal2">
+      <h1>this is 2nd modal</h1>
+      <template v-slot:links>
+        <a href="/">Sign Up</a> <br><br>
+        <a href="/">Sign In</a>
+      </template>
+    </Modal>
+  </div>
+  
+  <br>
+  <button @click="toggleModal">Open Modal</button>
+    <button @click="toggleModal2">Open Modal2</button>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Modal from  "./components/Modal";
 
 export default {
+  data(){
+    return{
+      title:"salman",
+      header:"this is header prop",
+      footer:"this is footer prop",
+      isOpen:false,
+      isOpen2:false
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name);
+      const name=this.$refs.name;
+      name.classList.add("active");
+    },
+    toggleModal(){
+      this.isOpen=!this.isOpen
+    },
+    toggleModal2(){
+      this.isOpen2=!this.isOpen2
+    }
+  },
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Modal
   }
 }
 </script>
@@ -22,5 +67,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  color:seagreen;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:["header","footer"],
+    methods:{
+        closeModal(){
+            this.$emit("closeModal")
+        }
+    }
+}
+</script>
+
+<style scoped>
+    body{
+        margin: 0;
+        padding: 0;
+    }
+    .backdrop{
+        position: fixed;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+    }
+    .modal{
+        background:#fff;
+        width: 400px;
+        margin: 100px auto;
+        padding: 10px;
+        border-radius: 10px;
+    }
+</style>
